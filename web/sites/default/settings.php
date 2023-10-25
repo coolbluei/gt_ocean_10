bd3c1e72fc16d9bcf7a9aa424229d23d21afd0c1
David Smallwood <drsmallwood@gmail.com>
2023-10-25 15:46:04 -0400

    add platform specific config, and tools via composer update.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -32,6 +32,11 @@ $settings['file_scan_ignore_directories'] = [
 // This is defined inside the read-only "config" directory, deployed via Git.
 $settings['config_sync_directory'] = '../config';
 
+// Automatic Platform.sh settings.
+if (file_exists($app_root . '/' . $site_path . '/settings.platformsh.php')) {
+  include $app_root . '/' . $site_path . '/settings.platformsh.php';
+}
+
 // Local settings. These come last so that they can override anything.
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
