e32a9c3c467cf7e01e3f73c93404884059cf4981
Franco Jara <franco@easytechgreen.com>
2023-11-10 15:12:43 -0300

    Update home block ID

--- web/modules/custom/gt_ocean/src/Plugin/Block/HomePageHeroBlock.php
+++ web/modules/custom/gt_ocean/src/Plugin/Block/HomePageHeroBlock.php
@@ -5,10 +5,10 @@ namespace Drupal\gt_ocean\Plugin\Block;
 use Drupal\Core\Block\BlockBase;
 
 /**
- * Provides a 'Hello' Block.
+ * Provides a Hero Banner Home Block.
  *
  * @Block(
- *   id = "home_page_hero_block",
+ *   id = "gt_ocean_home_hero_block",
  *   admin_label = @Translation("Home Page Hero Block"),
  *   category = @Translation("Custom"),
  * )
